0b0651dac6e8a2208e0c414bf86dfa1b3dac5f95
benhtodd <benhtodd@gmail.com>
2024-12-17 16:27:47 -0800

    update

--- powershell/vmservice2.ps1
+++ powershell/vmservice2.ps1
@@ -1,4 +1,6 @@
 # # Define the ESXi hosts and their credentials (if different)
+$NTPServer = "10.0.0.253" 
+
 $esxiHosts = @(
     @{ Hostname = "esxi-1.vcf.sddc.lab"; User = "root"; Password = "VMware123!VMware123!" },
     @{ Hostname = "esxi-2.vcf.sddc.lab"; User = "root"; Password = "VMware123!VMware123!" },
@@ -13,8 +15,12 @@ foreach ($esxiHost in $esxiHosts) {
     try {
         Connect-VIServer -Server $hostname -User $user -Password $password -ErrorAction Stop
         Get-VMHost $hostname | Get-VMHostService | Where-Object { $_.Key -eq "TSM-SSH" } | Start-VMHostService
+        Add-VMHostNtpServer -VMHost $hostname -NtpServer $NTPServer -Confirm:$false
+        Get-VMHostService -VMHost $hostname | Where-Object {$_.Key -eq "ntpd"} | Start-VMHostService -Confirm:$false
+
         Disconnect-VIServer -Confirm:$false
         Write-Host "SSH started on $hostname"
+        Write-Host "NTPD started on $hostname"
     }
     catch {
         Write-Error "Error on " $hostname $($_.Exception.Message)
